f02467a2e19218e5d3378ec48979702d3e4c85b4
Mrinmoy Sadhukhan <72397091+mrinmoy-sadhukhan@users.noreply.github.com>
2024-12-27 18:54:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ import cv2
 import cuhk03_dataset
 xrange=range
 FLAGS = tf.compat.v1.flags.FLAGS
-tf.flags.DEFINE_integer('batch_size', '150', 'batch size for training')
-tf.flags.DEFINE_integer('max_steps', '210000', 'max steps for training')
-tf.flags.DEFINE_string('logs_dir', 'logs/', 'path to logs directory')
-tf.flags.DEFINE_string('data_dir', 'data/', 'path to dataset')
-tf.flags.DEFINE_float('learning_rate', '0.01', '')
-tf.flags.DEFINE_string('mode', 'train', 'Mode train, val, test')
-tf.flags.DEFINE_string('image1', '', 'First image path to compare')
-tf.flags.DEFINE_string('image2', '', 'Second image path to compare')
+tf.compat.v1.flags.DEFINE_integer('batch_size', '150', 'batch size for training')
+tf.compat.v1.flags.DEFINE_integer('max_steps', '210000', 'max steps for training')
+tf.compat.v1.flags.DEFINE_string('logs_dir', 'logs/', 'path to logs directory')
+tf.compat.v1.flags.DEFINE_string('data_dir', 'data/', 'path to dataset')
+tf.compat.v1.flags.DEFINE_float('learning_rate', '0.01', '')
+tf.compat.v1.flags.DEFINE_string('mode', 'train', 'Mode train, val, test')
+tf.compat.v1.flags.DEFINE_string('image1', '', 'First image path to compare')
+tf.compat.v1.flags.DEFINE_string('image2', '', 'Second image path to compare')
 
 IMAGE_WIDTH = 60
 IMAGE_HEIGHT = 160
